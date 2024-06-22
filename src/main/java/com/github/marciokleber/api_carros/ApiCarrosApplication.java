db4e60d67f1eed665fd5655da1de096bac882353
Marcio Kleber Junior Lopes de Souza <marciokjls@gmail.com>
2024-06-22 12:00:36 -0300

    first commit - Criação de api rest de carros.

---
+++ src/main/java/com/github/marciokleber/api_carros/ApiCarrosApplication.java
@@ -0,0 +1,13 @@
+package com.github.marciokleber.api_carros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCarrosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiCarrosApplication.class, args);
+    }
+
+}
